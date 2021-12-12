5081ad4d1948cad10bedce4f32449990a3bf0b87
DF_Tv <61697853+DRTFTv@users.noreply.github.com>
2021-12-12 03:36:53 -0300

    Update

--- componets/main.js
+++ componets/main.js
@@ -1,9 +1,12 @@
 function confirming() {
     var name = prompt("Type your name:");
-
+    
+    //To add phone number linked to whatsapp
+    var phone = "5500000000000";
+    
     if (name == null || name == "") {
         alert("Name not entered!");
     } else {
-        window.location.href = `https://api.whatsapp.com/send?phone=5500000000000&text=I%20${name}%20confirm%20my%20presence%20at%20the%20halloween%20party`;
+        window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=I%20${name}%20confirm%20my%20presence%20at%20the%20halloween%20party`;
     }
 }
